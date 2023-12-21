28bcffb629289e492d9c3476cc6cc43905ab951f
deelaka99 <123wejith@gmail.com>
2023-12-21 23:05:16 +0530

    added modal to notify 'enter item details'

--- src/components/main_container/PricingPanel.jsx
+++ src/components/main_container/PricingPanel.jsx
@@ -7,6 +7,8 @@ function PricingPanel(props) {
   const [name, setName] = useState("");
   const [qty, setQty] = useState("");
   const [unitPrice, setUnitPrice] = useState("");
+  const [showItemAddedUnsucessModal, setShowItemAddedUnsucessModal] =
+    useState(false);
 
   useEffect(() => {
     setId(props.pricingId);
@@ -36,7 +38,7 @@ function PricingPanel(props) {
       // Clear the input fields
       clear();
     } else {
-      console.error("Incomplete item data. Cannot add to the array.");
+      setShowItemAddedUnsucessModal(true);
     }
   };
 
@@ -115,6 +117,39 @@ function PricingPanel(props) {
           </div>
         </div>
       </div>
+
+      {/**Item adding unsucess modal */}
+      {showItemAddedUnsucessModal ? (
+        <div>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none text-white">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="p-5 rounded-lg shadow-lg relative flex flex-col w-full bg-green border-2 outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-2 rounded-t">
+                  <h3 className="text-sm">Notification</h3>
+                  <button
+                    className="ml-auto bg-red rounded-sm border-0 text-lg font-semibold drop-shadow-md active:bg-white"
+                    onClick={() => setShowItemAddedUnsucessModal(false)}
+                  >
+                    <span className=" drop-shadow-lg shadow-black h-6 w-6 text-white flex items-center justify-center active:text-dark-ternary">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex flex-col">
+                  <h3 className="text-2xl font-semibold text-center">
+                    Enter Item details first! 😢
+                  </h3>
+                </div>
+                {/*footer*/}
+              </div>
+            </div>
+          </div>
+          <div className="rounded-lg opacity-50 fixed inset-0 z-40 bg-black"></div>
+        </div>
+      ) : null}
     </div>
   );
 }
