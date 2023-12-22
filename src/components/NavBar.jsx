5bb57e8e2fc7d7bf5e7a2c470b6cb3210c4b9061
deelaka99 <123wejith@gmail.com>
2023-12-22 17:14:01 +0530

    added changes to the navbar's responsiveness

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -34,33 +34,33 @@ function NavBar() {
         <img
           src={Logo}
           alt="Logo"
-          className="tablet:h-[60px] tablet:w-[120px] laptop:h-[75px] laptop:w-[150px] desktop:h-[65px] desktop:w-[130px]"
+          className="tablet:h-[60px] tablet:w-[120px] laptop:h-[50px] laptop:w-[100px] desktop:h-[60px] desktop:w-[120px]"
         />
       </div>
       <div className="h-full w-1/3 flex justify-center items-center">
-        <p className="font-inter font-semibold text-white tablet:text-[18px] laptop:text-[22px] desktop:text-[18px]">
+        <p className="font-inter font-semibold text-white tablet:text-[18px] laptop:text-[18px] desktop:text-[18px]">
           {date.toDateString()}
         </p>
       </div>
       <div className="h-full w-1/3 flex">
         <div className="h-full w-2/4 flex justify-left items-center pl-2">
-          <p className="font-inter text-white tablet:text-[15px] laptop:text-[18px] desktop:text-[14px]">
+          <p className="font-inter text-white tablet:text-[15px] laptop:text-[12px] desktop:text-[14px]">
             {"Good "+greet+"! Peter Jhon"}
           </p>
         </div>
         <div className="h-full w-1/4 tablet:p-2 flex items-center justify-center">
-          <div className="bg-white tablet:h-[55px] tablet:w-[55px] desktop:h-[44px] desktop:w-[44px] rounded-full shadow-black shadow-sm flex items-center justify-center">
+          <div className="bg-white tablet:h-[55px] tablet:w-[55px] laptop:h-[42px] laptop:w-[42px] desktop:h-[44px] desktop:w-[44px] rounded-full shadow-black shadow-sm flex items-center justify-center">
             <img
               src={User}
               alt="user"
-              className="tablet:h-[53px] tablet:w-[53px] desktop:h-[42px] desktop:w-[42px] rounded-full"
+              className="tablet:h-[53px] tablet:w-[53px] laptop:h-[40px] laptop:w-[40px] desktop:h-[42px] desktop:w-[42px] rounded-full"
             />
           </div>
         </div>
         <div className="h-full w-1/4 flex items-center justify-center">
           <button
             onClick={logout}
-            className="bg-red tablet:h-[35px] tablet:w-[50px] tablet:text-[12px] laptop:h-[40px] laptop:w-[60px] laptop:text-[15px] text-white rounded-lg shadow-black shadow-sm"
+            className="bg-red tablet:h-[35px] tablet:w-[50px] tablet:text-[12px] laptop:h-[30px] laptop:w-[60px] laptop:text-[12px] text-white rounded-lg shadow-black shadow-sm"
           >
             Logout
           </button>
