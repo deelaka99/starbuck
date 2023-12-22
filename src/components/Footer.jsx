58704eeea4db36455ba97be25323989c06cc7c3d
deelaka99 <123wejith@gmail.com>
2023-12-22 15:47:11 +0530

    added a countdown to close the payment acceptance modal

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,10 @@ function Footer(props) {
   const [discount, setDiscount] = useState(10);
   const [itemCount, setItemCount] = useState(0);
   const [clearAll, setClearAll] = useState(false);
+  const [closeCountDown, setCloseCountDown] = useState(5);
   const [showPaymentAcceptanceModal, setShowPaymentAcceptanceModal] =
+    useState(false);
+  const [showPaymentNotAcceptanceModal, setShowPaymentNotAcceptanceModal] =
     useState(false);
 
   useEffect(() => {
@@ -42,6 +45,25 @@ function Footer(props) {
     }
   }, [props.broughtItems, items, clearAll]);
 
+  useEffect(() => {
+    let countdownInterval;
+
+    // Start countdown when modal is open
+    if (showPaymentAcceptanceModal && closeCountDown > 0) {
+      countdownInterval = setInterval(() => {
+        setCloseCountDown((prevCount) => prevCount - 1);
+      }, 1000);
+    }
+
+    // Clear interval when modal is closed or countdown reaches 0
+    if (!showPaymentAcceptanceModal || closeCountDown == 0) {
+      clearInterval(countdownInterval);
+    }
+
+    // Clear interval on component unmount
+    return () => clearInterval(countdownInterval);
+  }, [showPaymentAcceptanceModal, closeCountDown]);
+
   return (
     <div className="bg-green w-full tablet:h-[12vh] laptop:h-[14vh] desktop:h-[14vh]">
       <div className="flex w-full tablet:h-[9vh] laptop:h-[11vh] desktop:h-[11vh]">
@@ -63,7 +85,13 @@ function Footer(props) {
         <div className="h-full w-1/3">
           <div className="h-full w-full flex items-center justify-end tablet:pr-4 laptop:pr-8">
             <button
-              onClick={() => setShowPaymentAcceptanceModal(true)}
+              onClick={() => {
+                if (items.length > 0) {
+                  setShowPaymentAcceptanceModal(true);
+                } else {
+                  setShowPaymentNotAcceptanceModal(true);
+                }
+              }}
               className="bg-darkGreen rounded-lg tablet:p-6 laptop:p-8 desktop:p-4 text-white font-inter font-semibold tablet:text-[20px] laptop:text-[24px] desktop:text-[20px] shadow-black shadow-md hover:shadow-black hover:shadow-lg"
             >
               Proceed to Pay
@@ -76,7 +104,8 @@ function Footer(props) {
           © PDW Kariyawasam
         </p>
       </div>
-      {/**Item adding unsucess modal */}
+
+      {/**Payment success modal */}
       {showPaymentAcceptanceModal ? (
         <div>
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none text-white">
@@ -104,7 +133,6 @@ function Footer(props) {
                   <div className="flex items-center justify-center gap-7">
                     <button
                       onClick={() => {
-                        console.log("Yes button clicked");
                         setClearAll(true);
                         props.clearAllItems();
                       }}
@@ -115,12 +143,56 @@ function Footer(props) {
                     <button
                       onClick={() => {
                         setShowPaymentAcceptanceModal(false);
+                        setCloseCountDown(5);
                       }}
                       className="font-inter font-medium text-xl text-red active:text-grey"
                     >
-                      No <span>(5)</span>
+                      No
+                      <span>
+                        (
+                        {closeCountDown === 0
+                          ? (() => {
+                              setShowPaymentAcceptanceModal(false);
+                              setCloseCountDown(5);
+                            })()
+                          : `${closeCountDown}s`}
+                        )
+                      </span>
                     </button>
                   </div>
+                </div>
+                {/*footer*/}
+              </div>
+            </div>
+          </div>
+          <div className="rounded-lg opacity-50 fixed inset-0 z-40 bg-black"></div>
+        </div>
+      ) : null}
+
+      {/**Payement not acceptance modal */}
+      {showPaymentNotAcceptanceModal ? (
+        <div>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none text-white">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="p-5 rounded-lg shadow-lg relative flex flex-col w-full bg-green border-2 outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-2 rounded-t">
+                  <h3 className="text-sm">Notification</h3>
+                  <button
+                    className="ml-auto bg-red rounded-sm border-0 text-lg font-semibold drop-shadow-md active:bg-white"
+                    onClick={() => setShowPaymentNotAcceptanceModal(false)}
+                  >
+                    <span className=" drop-shadow-lg shadow-black h-6 w-6 text-white flex items-center justify-center active:text-grey">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex flex-col">
+                  <h3 className="text-2xl font-semibold text-center">
+                    Please add items first!
+                  </h3>
                 </div>
                 {/*footer*/}
               </div>
